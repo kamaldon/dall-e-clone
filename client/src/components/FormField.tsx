ccf6161b15369fed1d789f742593c96bfa88e6c7
Kevin Maldonado <kevin.maldonado@sidewalklabs.com>
2023-02-20 23:42:51 +0000

    enhancing base templates for each page

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,7 +1,53 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+type SurpriseMeProps =
+  | {
+      isSurpriseMe: true;
+      handleSurpriseMe: () => void;
+    }
+  | {
+      isSurpriseMe?: false;
+    };
+
+type Props = {
+  labelName: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  handleChange?: (e: any) => void;
+} & SurpriseMeProps;
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  ...props
+}: Props) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+        {props.isSurpriseMe && (
+          <button
+            type="button"
+            onClick={props.handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default FormField;
--- client/src/pages/CreatePost.tsx
+++ client/src/pages/CreatePost.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FormField } from "../components";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  const [form, setForm] = useState({ name: "", prompt: "", photo: "" });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = () => {};
+  const handleChange = (e: any) => {};
+  const handleSurpriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w[500px]">
+          Create imaginative and visually stunning images through by DALLE-E AI
+          and share them with the community.
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="My name"
+            isSurpriseMe={false}
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A plush toy robot sitting against a yellow wall"
+            value={form.prompt}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+            handleChange={handleChange}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default CreatePost;
